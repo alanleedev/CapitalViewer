71de06546424dc45ad34ebb37c89889714187fc2
Alan Lee <byungho.alan.lee@gmail.com>
2023-12-04 21:13:30 -0800

    properly load fragment from activity

--- app/src/main/java/project/wgt/capitalviewer/MainActivity.kt
+++ app/src/main/java/project/wgt/capitalviewer/MainActivity.kt
@@ -7,13 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val mainFragment = MainFragment.newInstance()
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.fragmentContainerView, mainFragment,
-                MainFragment.TAG
-            )
-            .commit()
     }
 }
