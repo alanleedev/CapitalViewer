d13ae525385c25e24f1510b55bac59cd77ac934c
Alan Lee <byungho.alan.lee@gmail.com>
2023-12-04 23:21:25 -0800

    fetch data from repository and convert to presentation model

--- app/src/main/java/project/wgt/capitalviewer/model/CountryItem.kt
+++ app/src/main/java/project/wgt/capitalviewer/model/CountryItem.kt
@@ -1,8 +1,12 @@
 package project.wgt.capitalviewer.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CountryItem(
     val name: String,
     val region: String,
     val code: String,
     val capital: String,
-)
+): Parcelable
--- app/src/main/java/project/wgt/capitalviewer/repository/CapitalRepository.kt
+++ app/src/main/java/project/wgt/capitalviewer/repository/CapitalRepository.kt
@@ -1,28 +1,40 @@
 package project.wgt.capitalviewer.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import project.wgt.capitalviewer.di.Dependencies
 import project.wgt.capitalviewer.model.CountryItem
 import project.wgt.capitalviewer.network.CapitalAPIService
+import project.wgt.capitalviewer.network.model.CountryResponse
 
-class CapitalRepository(private val service: CapitalAPIService) {
+class CapitalRepository(
+    private val service: CapitalAPIService,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
 
-    suspend fun getCapital(): List<CountryItem> {
-        return service.getCountries().map { country ->
-            CountryItem(
-                country.name,
-                country.region,
-                country.code,
-                country.capital,
-            )
+    suspend fun getCountries(): List<CountryItem> {
+        return withContext(ioDispatcher) {
+            service.getCountries().toCountryItems()
         }
     }
 }
 
+fun List<CountryResponse>.toCountryItems(): List<CountryItem> =
+    this.map { country ->
+        CountryItem(
+            country.name,
+            country.region,
+            country.code,
+            country.capital,
+        )
+    }
+
 fun main() {
     runBlocking {
         // quick test repository
-        val countries = Dependencies.repository.getCapital()
+        val countries = Dependencies.repository.getCountries()
         println(countries)
     }
 }
