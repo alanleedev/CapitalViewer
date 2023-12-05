7a460a98e0151c1af9b2b698a1274dcc4634fa61
Alan Lee <byungho.alan.lee@gmail.com>
2023-12-04 20:20:42 -0800

    add retrofit and CapitalAPIService

---
+++ app/src/main/java/project/wgt/capitalviewer/di/Dependencies.kt
@@ -0,0 +1,26 @@
+package project.wgt.capitalviewer.di
+
+import com.google.gson.GsonBuilder
+import project.wgt.capitalviewer.network.CapitalAPIService
+import project.wgt.capitalviewer.util.Constants
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+// lazily create and store global dependencies used in the app
+object Dependencies {
+
+    private val retrofit: Retrofit by lazy {
+        val gson = GsonBuilder().create()
+        Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(Constants.BASE_URL)
+            .build()
+    }
+
+    val service: CapitalAPIService by lazy {
+        retrofit.create(CapitalAPIService::class.java)
+    }
+}
