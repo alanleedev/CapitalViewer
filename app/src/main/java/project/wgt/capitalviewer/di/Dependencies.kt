05569fc08a45c0b54fbb3e5d27170e435cf03789
Alan Lee <byungho.alan.lee@gmail.com>
2023-12-04 23:13:01 -0800

    cleanup retrofit

--- app/src/main/java/project/wgt/capitalviewer/di/Dependencies.kt
+++ app/src/main/java/project/wgt/capitalviewer/di/Dependencies.kt
@@ -5,7 +5,6 @@ import project.wgt.capitalviewer.network.CapitalAPIService
 import project.wgt.capitalviewer.repository.CapitalRepository
 import project.wgt.capitalviewer.util.Constants
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
@@ -15,7 +14,6 @@ object Dependencies {
     private val retrofit: Retrofit by lazy {
         val gson = GsonBuilder().create()
         Retrofit.Builder()
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gson))
             .baseUrl(Constants.BASE_URL)
             .build()
